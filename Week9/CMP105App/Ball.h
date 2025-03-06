ae05175402898f9ccc8b2e757b2bf83adbcc5370
2401312 <2401312@uad.ac.uk>
2025-03-06 09:26:03 +0000

    Created the classes

---
+++ Week9/CMP105App/Ball.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Framework/GameObject.h"
+#include "SFML/Graphics.hpp"
+
+class Ball : public GameObject
+{
+public: 
+	Ball();
+	~Ball();
+
+	void update(float dt);
+
+private : 
+
+
+};
+
---
+++ Week9/CMP105App/Ball_Manager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Ball.h"
+#include <math.h>
+#include <vector>
+
+class Ball_Manager
+{
+
+public: 
+
+	Ball_Manager();
+	~Ball_Manager();
+
+	void spawn();
+	void update(float dt);
+	void deathCheck();
+	void render(sf::RenderWindow* window);
+
+private:
+
+	std::vector<Ball> Balls;
+	sf::Vector2f Spawn_Point;
+
+};
+
